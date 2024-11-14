6586e5d99df6ed87086ddaf43739dd19c8388508
user <user@DESKTOP-FTP7DR2>
2024-11-14 14:17:13 +0900

    HealthInfo sql 문 수정 + 시퀀스 추가

--- someBodyHealthMe/src/main/webapp/sql/HY.sql
+++ someBodyHealthMe/src/main/webapp/sql/HY.sql
@@ -1,14 +1,21 @@
+-- 테이블 생성
 CREATE TABLE Healthinfo (
-    HealthInfoID NUMBER NOT NULL COMMENT '건강 정보 고유 코드, PK',
-    Height NUMBER(5, 2) NOT NULL COMMENT '키 (단위: cm)',
-    Weight NUMBER(5, 2) NOT NULL COMMENT '몸무게 (단위: kg)',
-    Age NUMBER(3) NOT NULL COMMENT '사용자 나이 데이터',
-    BMI NUMBER(5, 2) NOT NULL COMMENT '사용자 BMI 지수 데이터',
-    Goal VARCHAR2(50) NOT NULL COMMENT '운동 목표 (다이어트, 벌크업, 유지 등)',
-    Gender CHAR(1) NOT NULL COMMENT '성별 (M: 남성, F: 여성)',
-    CreatedAt DATE NOT NULL COMMENT '데이터 생성 시간',
-    ModifyDate DATE NULL COMMENT '수정일',
-    user_num NUMBER NOT NULL COMMENT '각 사용자를 구별하는 고유 ID',
+    HealthInfoID NUMBER NOT NULL,
+    Height NUMBER(5, 2) NOT NULL,
+    Weight NUMBER(5, 2) NOT NULL,
+    Age NUMBER(3) NOT NULL,
+    BMI NUMBER(5, 2) NOT NULL,
+    Goal VARCHAR2(50) NOT NULL,
+    Gender CHAR(1) NOT NULL,
+    CreatedAt DATE NOT NULL,
+    ModifyDate DATE NULL,
+    user_num NUMBER NOT NULL,
     CONSTRAINT Healthinfo_PK PRIMARY KEY (HealthInfoID)
-) COMMENT '건강 정보 테이블';
+);
 
+-- 시퀀스 생성 (HealthInfoID 자동 증가)
+CREATE SEQUENCE Healthinfo_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;

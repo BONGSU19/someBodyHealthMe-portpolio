bd7f48142542f220e2bd0e7e8afc7424977a9fbf
user <user@DESKTOP-VA2847C>
2024-12-03 14:16:30 +0900

    오류수정

--- someBodyHealthMe/src/main/java/kr/goods/action/DetailAction.java
+++ someBodyHealthMe/src/main/java/kr/goods/action/DetailAction.java
@@ -15,6 +15,10 @@ public class DetailAction implements Action{
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		HttpSession session = request.getSession();
+		Long user_num = (Long)session.getAttribute("user_num");
+		if(user_num == null) {//로그인 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
 		Integer status = (Integer)session.getAttribute("status");
 		// 상품번호 반환
 		long goods_num = Long.parseLong(request.getParameter("goods_num"));
